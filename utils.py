ed541e494fbc6640f07fa92e72d56d36bcb1ef3f
Enrico <enrico@stat.unipd>
2025-01-09 15:31:36 +0100

    eliminazione funzioni inutilizzate

--- utils.py
+++ utils.py
@@ -3,7 +3,6 @@ from dataframes.medals import olympics
 from dataframes.city import cities
 from dataframes.italy2024 import italy
 import dataframes.countries as countries
-from PIL import Image
 import geopandas as gpd
 import os
 
@@ -25,10 +24,6 @@ def setup_page(title, icon, layout = "centered", css_file = "style/style.css"):
 def local_css(file):
     with open(file) as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html = True)
-
-def resize_image(image_path, width, height):
-    img = Image.open(image_path)
-    return img.resize((width, height))
 
 def get_geography():
     path = "map/ne_10m_admin_0_countries.zip"
